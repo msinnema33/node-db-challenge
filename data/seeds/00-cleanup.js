84565b6c10b253e8c53e7245634435b656eae150
Mike Sinnema <msinnema33@gmail.com>
2020-03-20 10:01:05 -0700

    removed cleanup seed for troubleshooting

--- data/seeds/00-cleanup.js
+++
@@ -1,9 +0,0 @@
-const cleaner = requires('knex-cleaner');
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return cleaner.clean(knex, {
-    mode: 'truncate',
-    ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
-    });
-};
